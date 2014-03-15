b86a5ffbe8c82569e9541987f3478b4d9162df54
eraindil <KiracorpNivoliez@gmail.com>
2014-03-15 20:56:33 +0100

    Ajout: Supression: --aucun-- Modification: --aucun--

---
+++ model/Operation.cpp
@@ -0,0 +1,5 @@
+#include "operation.h"
+
+operation::operation()
+{
+}
---
+++ model/Operation.h
@@ -0,0 +1,10 @@
+#ifndef OPERATION_H
+#define OPERATION_H
+
+class operation
+{
+public:
+    operation();
+};
+
+#endif // OPERATION_H
